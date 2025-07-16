563a4fc39350cd91442d763d3d85f28f76022ad9
Jayaditya Ranjan <101096637+JAYADITYA83@users.noreply.github.com>
2025-07-16 20:57:31 +0530

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 const clientId = process.env.CLIENT_ID;
